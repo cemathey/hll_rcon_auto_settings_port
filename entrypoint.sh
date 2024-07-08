e5057616f05de72e3efc0ac9932f233207630c3d
C. Eric Mathey <emathey@protonmail.com>
2024-07-08 11:49:21 -0600

    Fix port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 set -x 
 
-poetry run gunicorn -t 120 -b 0.0.0.0:8080 --chdir hll_rcon_auto_settings_port app:app
+poetry run gunicorn -t 120 -b 0.0.0.0:8000 --chdir hll_rcon_auto_settings_port app:app
